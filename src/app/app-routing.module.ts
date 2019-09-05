eee4c997afbe084eb274cde7b1175a3782ff585c
jhonatan.leon <jhonatan.leon@farmatodo.com>
2019-09-04 22:43:48 -0500

    se agrega componente de home con slider

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: HomeComponent},
+  { path: '', pathMatch: 'full', redirectTo: 'inicio'},
+  { path: '**', pathMatch: 'full', redirectTo: 'inicio' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
